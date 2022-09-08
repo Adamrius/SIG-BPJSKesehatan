b22cba5c8de84cc1a59ebc1e17510efe5cd98dae
Adamrius Situmorang <damnteamcorp@gmail.com>
2022-09-08 22:34:59 +0700

    update ui front

--- application/views/home.php
+++ application/views/home.php
@@ -1,9 +1,11 @@
 <section class="section-maps">
     <div class="container-fluid gx-0">
-        <div class="top-section">
-            <div class="container">
-                <h2>PEMETAAN FASILITAS KESEHATAN BPJS WILAYAH BANYUMAS</h2>
-            </div>
+        <div class="top-section text-dark">
+            <!-- <div class="container"> -->
+            <marquee direction="left" scrollamount="9" bgcolor='#00a2ff '>
+                <h1>INFORMASI FASILITAS BPJS KESEHATAN TINGKAT PERTAMA KABUPATEN BANYUMAS</h1>
+            </marquee>
+            <!-- </div> -->
         </div>
 
         <div class="content-area">
@@ -45,8 +47,8 @@
                     $.each(response.data, function(i, val) {
                         data_faskes +=
                             '<a class="list-item trigg-filter" data="' + val.id + '">' +
-                            // '<div class="marker"><i class="fa fa-hospital" style="color: ' + val.color + ';"></i></div>' +
-                            '<div class="marker"><i class="fa fa-map-marker-alt" style="color: ' + val.color + ';"></i></div>' +
+                            '<div class="marker"><i class="fa fa-hospital" style="color: ' + val.color + ';"></i></div>' +
+                            // '<div class="marker"><i class="fa fa-map-marker-alt" style="color: ' + val.color + ';"></i></div>' +
                             '<div class="text section-description">' + val.name + ' (' + val.total + ')</div>' +
                             '</a>';
                     });
@@ -84,10 +86,11 @@
         function initMap(data_maps_array) {
             var options = {
                 zoom: 11,
-                center: new google.maps.LatLng(-7.4459451, 109.0489305),
+                center: new google.maps.LatLng(-7.450221641069655, 109.27323322638935),
                 mapTypeId: google.maps.MapTypeId.ROADMAP,
-                mapTypeControl: true,
-                streetViewControl: true
+                mapTypeControl: false,
+                streetViewControl: false,
+                disableDefaultUI: true,
             };
             var map = new google.maps.Map(document.getElementById('maps'), options);
             var data_maps = data_maps_array;
@@ -100,8 +103,8 @@
                     animation: google.maps.Animation.DROP,
                     draggable: false,
                     icon: {
-                        // path: "M448 492v20H0v-20c0-6.627 5.373-12 12-12h20V120c0-13.255 10.745-24 24-24h88V24c0-13.255 10.745-24 24-24h112c13.255 0 24 10.745 24 24v72h88c13.255 0 24 10.745 24 24v360h20c6.627 0 12 5.373 12 12zM308 192h-40c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12v-40c0-6.627-5.373-12-12-12zm-168 64h40c6.627 0 12-5.373 12-12v-40c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12zm104 128h-40c-6.627 0-12 5.373-12 12v84h64v-84c0-6.627-5.373-12-12-12zm64-96h-40c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12v-40c0-6.627-5.373-12-12-12zm-116 12c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12v-40zM182 96h26v26a6 6 0 0 0 6 6h20a6 6 0 0 0 6-6V96h26a6 6 0 0 0 6-6V70a6 6 0 0 0-6-6h-26V38a6 6 0 0 0-6-6h-20a6 6 0 0 0-6 6v26h-26a6 6 0 0 0-6 6v20a6 6 0 0 0 6 6z",
-                        path: "M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z",
+                        path: "M448 492v20H0v-20c0-6.627 5.373-12 12-12h20V120c0-13.255 10.745-24 24-24h88V24c0-13.255 10.745-24 24-24h112c13.255 0 24 10.745 24 24v72h88c13.255 0 24 10.745 24 24v360h20c6.627 0 12 5.373 12 12zM308 192h-40c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12v-40c0-6.627-5.373-12-12-12zm-168 64h40c6.627 0 12-5.373 12-12v-40c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12zm104 128h-40c-6.627 0-12 5.373-12 12v84h64v-84c0-6.627-5.373-12-12-12zm64-96h-40c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12v-40c0-6.627-5.373-12-12-12zm-116 12c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12v-40zM182 96h26v26a6 6 0 0 0 6 6h20a6 6 0 0 0 6-6V96h26a6 6 0 0 0 6-6V70a6 6 0 0 0-6-6h-26V38a6 6 0 0 0-6-6h-20a6 6 0 0 0-6 6v26h-26a6 6 0 0 0-6 6v20a6 6 0 0 0 6 6z",
+                        // path: "M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z",
                         fillColor: data_maps[i][3],
                         fillOpacity: 1,
                         strokeColor: "#ffffff",
